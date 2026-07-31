cea8456b6cfae8082370f45b8603143c4312b01d
Zachary_QwQ <1400619125@qq.com>
2026-07-31 10:54:47 +0800

    rename: update test import to irix package

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,9 @@
-// XMCServerLauncher 冒烟测试
+// IriX 冒烟测试
 // 验证应用根组件 MyApp 能够正常构建，不引用尚未创建的界面/状态。
 
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:xmcserverlauncher/main.dart';
+import 'package:irix/main.dart';
 
 void main() {
   testWidgets('MyApp 能够正常构建', (WidgetTester tester) async {
